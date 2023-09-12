d1731caebea1f0f10639b1e07083844a926e9fdc
Da Shen <da@tubi.tv>
2023-09-12 14:26:20 +0800

    pants polars sql runner

--- hello.py
+++ hello.py
@@ -1,12 +1,14 @@
-import pandas
+import polars as pl
+from pathlib import Path
+import sys
 
 def main():
-    try:
-        import pyspark
-        print(f"PySpark version {pyspark.__version__}")
-    except:
-        print("PySpark not available")
-
-    print(f"Hello with Pandas {pandas.__version__}")
+    path_to_sql = sys.argv[1]
+    context = pl.SQLContext()
+    sql_text = Path(path_to_sql).read_text()
+    print(sql_text)
+    print("---")
+    df = context.execute(sql_text)
+    print(df.collect())
 
 main()
